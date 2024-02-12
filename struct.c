da8531c720c3e4d49b0aad0f00c6bf387858bf6d
Maxime M <MaxiCom.Developpement@gmail.com>
2024-02-12 20:11:13 +0100

    struct.c

--- struct.c
+++ struct.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 // What are C structs (Structures)
 
 // With structs, you can group related variables in one place.
@@ -8,16 +10,26 @@
 // struct Keyword
 // declare each membre inside curly braces
 
-struct  MyStructure {    // Structure declaration
+struct  myStructure {    // Structure declaration
     int     myNum;      // Member (int variable)
     char    myLetter;   // My letter
 };                      // End the structure with a semicolon
 
 // Use the struct keyword inside the main() METHOD, followed by the name of the structure
 // and then the name of the structure variable
+
+// To access members of the struct, use the dot syntax (.)
+
 int     main()
 {
-    struct MyStructure s1;
+    struct myStructure s1;
+
+		s1.myNum = 123;
+		s1.myLetter = 'a';
+
+		// Print format
+		printf("My number: %d\n", s1.myNum);
+		printf("My char: %c\n", s1.myLetter);
 
     return 0;
 }
