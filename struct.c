9ccf5a353877f793054cbb2fab67cf7088f50adf
Maxime M <MaxiCom.Developpement@gmail.com>
2024-02-12 19:36:58 +0100

    +struct.c

---
+++ struct.c
@@ -0,0 +1,14 @@
+// What are C structs (Structures)
+
+// With structs, you can group related variables in one place.
+// Each variable in the struct is know as a MEMBER of the struct.
+
+// Unlike an array, a struct can contain different data type (int, float, char)
+
+// struct Keyword
+// declare each membre inside curly braces
+
+struct MyStructure {    // Structure declaration
+    int     myNum;      // Member (int variable)
+    char    myLetter;   // My letter
+};                      // End the structure with a semicolon
